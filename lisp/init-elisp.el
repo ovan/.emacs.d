8eea43f16268e84385e51e5e2e7103b0b1f0ade1
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-01-10 22:18:44 +0200

    use extended evil words in emacs-list-mode

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -1,7 +1,7 @@
 ;; Emacs Lisp mode customizations
 ;;
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'emacs-lisp-mode-hook 'my-evil/enable-symbol-word-search)
+(add-hook 'emacs-lisp-mode-hook 'my-evil/extend-word-definition)
 (add-hook 'emacs-lisp-mode-hook 'show-smartparens-mode)
 (add-hook 'emacs-lisp-mode-hook 'setup-highlighting-whitespace)
 
