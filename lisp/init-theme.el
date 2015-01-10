fdec838962eb36000c374c4da4650ac2479eefe7
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-01-10 21:27:09 +0200

    Ensure we always use solarized *dark*, not light

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,13 +1,12 @@
 (require-package 'color-theme-solarized)
 
-;; Load the solarized dark. Handle starting as daemon by only loading the theme
-;; when a frame has been created.
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-              (lambda (f)
-                (with-selected-frame f
-                  (set-frame-parameter f 'background-mode 'dark)
-                  (load-theme 'solarized t)))))
+;; Use dark version always
+(setq frame-background-mode 'dark)
+
+;; Handle starting as daemon by only loading the theme when a frame has been created.
+(add-hook 'after-make-frame-functions
+          (lambda (f)
+            (load-theme 'solarized t)))
 
 ;; On Mac, change the font to Inconsolata
 (when (eq system-type 'darwin)
@@ -16,12 +15,6 @@
 
 ; Reset background color of whitespace-mode to work with solarized
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :width normal :family "Inconsolata"))))
  '(whitespace-space ((t (:foreground "#073642")))))
 
 (provide 'init-theme)
-
