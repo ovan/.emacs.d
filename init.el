7d20228d8df3d920688410768cbb6dc4ab255fa3
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-01-30 21:08:08 +0200

    add smart-mode-line

--- init.el
+++ init.el
@@ -4,6 +4,20 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+
+;;----------------------------------------------------------------------------
+;; Custom's stuff
+;;----------------------------------------------------------------------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))))
+
+
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
@@ -11,6 +25,7 @@
 (require 'init-basic-conf) ;; Basic global configurations like toolbar disabling, indent-tabs-mode etc.
 (require 'init-mac)
 (require 'init-theme)
+(require 'init-smart-mode-line)
 
 ;;----------------------------------------------------------------------------
 ;; Supporting modes and packages
@@ -37,4 +52,3 @@
 (require 'init-js)
 (require 'init-css)
 (require 'init-org)
-
---
+++ lisp/init-smart-mode-line.el
@@ -0,0 +1,6 @@
+(require-package 'smart-mode-line)
+
+(sml/setup)
+(sml/apply-theme 'respectful)
+
+(provide 'init-smart-mode-line)
