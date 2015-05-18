b5704ed5d1d75f44ab167639bd1610f4d06578f7
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-05-18 14:08:07 +0300

    enable rainbow-mode for css mode by default

--- lisp/init-css.el
+++ lisp/init-css.el
@@ -1,9 +1,13 @@
 ;; CSS / SCSS mode
 
 (require-package 'scss-mode)
+(require-package 'rainbow-mode)
 
 (setq css-indent-offset 2)
 (setq scss-compile-at-save nil)
+
+;; Enable rainbow mode in css mode always to highligh colors inline
+(add-hook 'css-mode-hook 'rainbow-mode)
 
 (provide 'init-css)
 
