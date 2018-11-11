af40594fddde96d97d90370bfc4a74fcf38471b5
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2018-11-11 11:18:18 +0200

    Fix theme loading to set safe them early enough

--- init.el
+++ init.el
@@ -8,6 +8,12 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+;; Safe theme has to be set before loading the theme via init-theme
+(custom-set-variables
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
@@ -49,13 +55,4 @@
 (require 'init-css)
 (require 'init-org)
 (require 'init-docker)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
 
